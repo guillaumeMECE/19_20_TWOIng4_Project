a9c7c610f07d9a62d916ff6776d243ce365e5f2f
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2019-11-19 22:45:31 +0100

    Clean code drawer

--- frontend/src/components/drawer/index.js
+++ frontend/src/components/drawer/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
+import './style.css';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import List from '@material-ui/core/List';
@@ -7,14 +8,11 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
 import DashboardIcon from '@material-ui/icons/Dashboard';
 import CreditCardIcon from '@material-ui/icons/CreditCard';
 import CompareArrowsIcon from '@material-ui/icons/CompareArrows';
@@ -26,24 +24,6 @@ const useStyles = makeStyles(theme => ({
     root: {
         display: 'flex',
     },
-    appBar: {
-        zIndex: theme.zIndex.drawer + 1,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    appBarShift: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    menuButton: {
-        marginRight: 36,
-    },
     hide: {
         display: 'none',
     },
@@ -53,6 +33,7 @@ const useStyles = makeStyles(theme => ({
         whiteSpace: 'nowrap',
     },
     drawerOpen: {
+        borderRightColor: "#252a41",
         backgroundColor: '#252a41',
         width: drawerWidth,
         transition: theme.transitions.create('width', {
@@ -61,6 +42,7 @@ const useStyles = makeStyles(theme => ({
         }),
     },
     drawerClose: {
+        borderRightColor: "#252a41",
         backgroundColor: '#252a41',
         transition: theme.transitions.create('width', {
             easing: theme.transitions.easing.sharp,
@@ -142,7 +124,7 @@ export default function MiniDrawer() {
                         {open === false ? <ChevronRightIcon className={classes.icon} /> : <ChevronLeftIcon className={classes.icon} />}
                     </IconButton>
                 </div>
-                <Divider />
+
                 <List>
                     {['Dashboard', 'Card', 'Transaction'].map((text, index) => (
                         <ListItem button key={text}>
@@ -158,30 +140,21 @@ export default function MiniDrawer() {
                 </List>
             </Drawer>
             <main className={classes.content}>
-                <div className={classes.toolbar} />
-                <Typography paragraph>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                    ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-                    facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-                    gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-                    donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                    adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-                    Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-                    imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-                    arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-                    donec massa sapien faucibus et molestie ac.
-        </Typography>
-                <Typography paragraph>
-                    Consequat mauris nunc congue nisi vitae suscipit. Fringilla est ullamcorper eget nulla
-                    facilisi etiam dignissim diam. Pulvinar elementum integer enim neque volutpat ac
-                    tincidunt. Ornare suspendisse sed nisi lacus sed viverra tellus. Purus sit amet volutpat
-                    consequat mauris. Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-                    vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra accumsan in. In
-                    hendrerit gravida rutrum quisque non tellus orci ac. Pellentesque nec nam aliquam sem et
-                    tortor. Habitant morbi tristique senectus et. Adipiscing elit duis tristique sollicitudin
-                    nibh sit. Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra maecenas
-                    accumsan lacus vel facilisis. Nulla posuere sollicitudin aliquam ultrices sagittis orci a.
-        </Typography>
+
+                <div className="container">
+                    <Typography paragraph color="primary">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+                            ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
+                            facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
+                            gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
+                            donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
+                            adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
+                            Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
+                            imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
+                            arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
+                            donec massa sapien faucibus et molestie ac.
+                    </Typography>
+                </div>
             </main>
         </div>
     );
