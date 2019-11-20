76b42a9f52a58dcbc943f3ef5e534eed29ef0609
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2019-11-20 20:02:07 +0100

    Clean code

--- frontend/src/components/drawer/index.js
+++ frontend/src/components/drawer/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import clsx from 'clsx';
 import './style.css';
-import Widget from '../widget'
 
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
