cd5504c23e3e00da5e3b2dc74d4d2dfa030a5517
ClaraBzk <38009877+ClaraBzk@users.noreply.github.com>
2019-11-20 20:47:01 +0100

    import form

--- frontend/src/components/drawer/index.js
+++ frontend/src/components/drawer/index.js
@@ -16,6 +16,7 @@ import DashboardIcon from '@material-ui/icons/Dashboard';
 import CreditCardIcon from '@material-ui/icons/CreditCard';
 import CompareArrowsIcon from '@material-ui/icons/CompareArrows';
 import Box from '@material-ui/core/Box';
+import Form from '../form/form';
 
 const drawerWidth = 240;
 
@@ -136,20 +137,11 @@ export default function MiniDrawer() {
             </Drawer>
             <main className={classes.content}>
 
-                <div className="container">
-                    <Typography paragraph color="primary">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-                            ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
-                            facilisis leo vel. Risus at ultrices mi tempus imperdiet. Semper risus in hendrerit
-                            gravida rutrum quisque non tellus. Convallis convallis tellus id interdum velit laoreet id
-                            donec ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl suscipit
-                            adipiscing bibendum est ultricies integer quis. Cursus euismod quis viverra nibh cras.
-                            Metus vulputate eu scelerisque felis imperdiet proin fermentum leo. Mauris commodo quis
-                            imperdiet massa tincidunt. Cras tincidunt lobortis feugiat vivamus at augue. At augue eget
-                            arcu dictum varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt. Lorem
-                            donec massa sapien faucibus et molestie ac.
-                    </Typography>
-                </div>
+                {/* <div className="container"> */}
+                            <Form>
+                                
+                            </Form>
+                {/* </div> */}
             </main>
         </div>
     );
