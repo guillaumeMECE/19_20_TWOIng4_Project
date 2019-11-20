b2f2ddedff7a57133b22ca1f9f2cdfe3681b0ea1
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2019-11-20 19:14:52 +0100

    Widget Component Create

---
+++ frontend/src/components/widget/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import './style.css';
+
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+export default class Widget extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
+    render() {
+
+        return (
+
+            <Box className="widget" boxShadow={2}>
+                <Typography component="div">
+                    <Box fontSize="h6.fontSize" m={1}>
+                        {this.props.title}
+                    </Box>
+                </Typography>
+                {this.props.child()}
+            </Box>
+        );
+    }
+}
+
+Widget.defaultProps = {
+    title: 'Widget Title',
+    child: function () { return < Typography component="div" style={{ color: '#000000' }} > Components</Typography>; }
+}
