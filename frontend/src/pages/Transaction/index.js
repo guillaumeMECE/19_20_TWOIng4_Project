db4bfcd0803a547152c882dcd99914a303b435c9
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2019-11-20 23:27:54 +0100

    Transaction Page

---
+++ frontend/src/pages/Transaction/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './style.css';
+
+
+function Transaction() {
+    return (
+        <div className="Transaction">
+            Transaction
+        </div>
+    );
+}
+
+export default Transaction;
