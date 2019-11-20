b84d84900dc43a675f9f171211bbf3fce84f237e
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2019-11-20 23:25:12 +0100

    App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,24 +3,26 @@ import './App.css';
 import Drawer from './components/drawer/index';
 import Hidden from '@material-ui/core/Hidden';
 import BottomNavigation from './components/bottomNavigation/index';
-import GridList from './components/gridList'
 import { Box } from '@material-ui/core';
+import Routes from './routes'
 
+const renderPage = () => {
+  return (<Routes />);
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Hidden xsDown>
-          <Drawer />
-        </Hidden>
-        <Hidden smUp>
-          <Box m={2}>
-            <GridList isSmall="true" />
-          </Box>
-          <BottomNavigation />
-        </Hidden>
-      </header>
+      <Hidden xsDown>
+        <Drawer child={() => renderPage()} />
+      </Hidden>
+      <Hidden smUp>
+        <Box m={2}>
+          {renderPage()}
+        </Box>
+        <BottomNavigation />
+      </Hidden>
+
     </div>
   );
 }
