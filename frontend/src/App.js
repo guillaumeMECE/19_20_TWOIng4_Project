9b7d7ea86e502a28682df02d37ccc9a45c13858d
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2019-11-20 20:01:43 +0100

    Add grid for small screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import Drawer from './components/drawer/index';
 import Hidden from '@material-ui/core/Hidden';
 import BottomNavigation from './components/bottomNavigation/index';
+import GridList from './components/gridList'
+import { Box } from '@material-ui/core';
 
 
 function App() {
@@ -13,6 +15,9 @@ function App() {
           <Drawer />
         </Hidden>
         <Hidden smUp>
+          <Box m={2}>
+            <GridList isSmall="true" />
+          </Box>
           <BottomNavigation />
         </Hidden>
       </header>
