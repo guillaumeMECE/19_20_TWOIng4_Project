0af7f203ac5e819762ad12fd5793325bc1d9c38c
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2019-11-21 17:53:44 +0100

    Change App for active nav

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,30 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
+import { withRouter } from 'react-router-dom';
 import Drawer from './components/drawer/index';
 import Hidden from '@material-ui/core/Hidden';
 import BottomNavigation from './components/bottomNavigation/index';
 import { Box } from '@material-ui/core';
-import Routes from './routes'
+import Routes from './routes';
 
 const renderPage = () => {
   return (<Routes />);
 }
 
-function App() {
-  return (
-    <div className="App">
-      <Hidden xsDown>
-        <Drawer child={() => renderPage()} />
-      </Hidden>
-      <Hidden smUp>
-        <Box m={2}>
-          {renderPage()}
-        </Box>
-        <BottomNavigation />
-      </Hidden>
+// function App() {
+//   return (
+//     <div className="App">
+//       <Hidden xsDown>
+//         <Drawer child={() => renderPage()} />
+//       </Hidden>
+//       <Hidden smUp>
+//         <Box m={2}>
+//           {renderPage()}
+//         </Box>
+//         <BottomNavigation />
+//       </Hidden>
+//     </div>
+//   );
+// }
 
-    </div>
-  );
+
+// export default const SomeComponent = withRouter(props => <App {...props} />);
+class App extends React.Component {
+  render() {
+    const { pathname } = this.props.location;
+    console.log('pathname: ', pathname);
+
+    return (
+      <div className="App">
+        <Hidden xsDown>
+          <Drawer child={() => renderPage()} pathname={pathname} />
+        </Hidden>
+        <Hidden smUp>
+          <Box m={2}>
+            {renderPage()}
+          </Box>
+          <BottomNavigation />
+        </Hidden>
+      </div>
+    );
+  };
 }
 
-export default App;
+// const App = withRouter(props => <AppClass {...props} />);
+
+export default withRouter(props => <App {...props} />);
