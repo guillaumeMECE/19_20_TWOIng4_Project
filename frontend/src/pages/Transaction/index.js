8972813d60688ce3de876715d311f212a4aa8b2d
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2019-11-21 18:59:06 +0100

    Update Transaction page

--- frontend/src/pages/Transaction/index.js
+++ frontend/src/pages/Transaction/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import './style.css';
-import Form from '../../components/form'
+import Form from '../../components/form';
+import CardPreview from '../../components/cardPreview';
+import Grid from '@material-ui/core/Grid';
 
 
 function Transaction() {
     return (
         <div className="Transaction">
-            <Form />
+            <Grid container spacing={3}>
+                <Grid item xs={12} sm={12} md={6} lg={6}>
+                    <Form />
+                </Grid>
+                <Grid item xs>
+                    <CardPreview />
+                </Grid>
+            </Grid>
         </div>
     );
 }
