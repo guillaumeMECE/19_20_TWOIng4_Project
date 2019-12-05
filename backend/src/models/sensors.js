74d2ed36bafd676bb8572cbeb524d4ed0990b026
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2019-12-05 17:40:26 +0100

    Sensor model rename attributes

--- backend/src/models/sensors.js
+++ backend/src/models/sensors.js
@@ -12,7 +12,7 @@ const attributes = {
         type: Schema.Types.ObjectId,
         required: true
     },
-    CreatedDate: {
+    creationDate: {
         type: String,
         required: true
     },
