e426815e158fc237034e0641e506517f87032bbd
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2019-12-05 17:08:20 +0100

    Add measure route

--- backend/src/routes/api.js
+++ backend/src/routes/api.js
@@ -11,7 +11,7 @@ const router = Router();
  * Controllers imports
  */
 // Ashtray IMPORT
-const { ReadAshtray, ReadOneAshtray, CreateAshtray, DeleteAshtray, ResetAshtray, UpdateAshtray } = require('@controllers');
+// const { ReadAshtray, ReadOneAshtray, CreateAshtray, DeleteAshtray, ResetAshtray, UpdateAshtray } = require('@controllers');
 
 // AUTH IMPORT
 const { RegisterUser, LoginUser } = require('@controllers');
@@ -25,7 +25,7 @@ const { CreateUserQuestion, ReadUserQuestions, ReadUserQuestion, UpdateUserQuest
 /**
  * Measure IMPORT
  */
-const { ReadMeasure, ReadOneMeasure, CreateMeasure, DeleteMeasure, ResetMeasure, UpdateMeasure } = require('@controllers');
+const { ReadMeasure, ReadOneMeasure, CreateMeasure, DeleteMeasure, UpdateMeasure } = require('@controllers');
 
 
 // MIDDLEWARES
@@ -39,19 +39,18 @@ router.post('/login', LoginUser);
 
 // Measure ROUTES
 router.post('/measure', CreateMeasure);
-// router.get('/measure/read/:id', ReadOneAshtray);
+router.get('/measure/:id', ReadOneMeasure);
+router.get('/measure', ReadMeasure);
+router.patch('/measure/:id', UpdateMeasure);
+router.delete('/measure/:id', DeleteMeasure);
+
+// TODO ROUTES
+// router.post('/ashtray/create', CreateAshtray);
+// router.get('/ashtray/read/:id', ReadOneAshtray);
 // router.get('/ashtray/read', ReadAshtray);
 // router.put('/ashtray/reset/:id', ResetAshtray);
 // router.patch('/ashtray/update/:id', UpdateAshtray);
 // router.delete('/ashtray/delete/:id', DeleteAshtray);
-
-// TODO ROUTES
-router.post('/ashtray/create', CreateAshtray);
-router.get('/ashtray/read/:id', ReadOneAshtray);
-router.get('/ashtray/read', ReadAshtray);
-router.put('/ashtray/reset/:id', ResetAshtray);
-router.patch('/ashtray/update/:id', UpdateAshtray);
-router.delete('/ashtray/delete/:id', DeleteAshtray);
 
 // QUESTION ROUTES 
 router.post('/question/add', middleware, CreateQuestion);
