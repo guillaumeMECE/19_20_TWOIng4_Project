d8f6438e27d1afbaafd43df02a21d25899098284
GuillaumeMaurin <guillaume.maurin@edu.ece.fr>
2019-12-09 11:57:45 +0100

    Finish route sensorsByLoc

--- backend/src/routes/controllers/kpi/sensorsByLocation.js
+++ backend/src/routes/controllers/kpi/sensorsByLocation.js
@@ -21,21 +21,13 @@ const secure = async (req) => {
  */
 const process = async (param) => {
 
-    // console.log('inputs: ', inputs);
-
     try {
         const res = await UserModel.find({}).select({ 'location': true }).lean().exec();
-        // eslint-disable
+
         for (let index = 0; index < res.length; index++) {
             // eslint-disable-next-line no-await-in-loop
             res[index].sensors = await SensorModel.find({ 'userID': res[index]._id }).count().exec();
-            // console.log('res[index].sensors: ', res[index].sensors);
         }
-        console.log('result: ', res);
-        // result.forEach(element => {
-        //     const res = await SensorModel.find({ 'userID': result._id }).exec();
-        // });
-
 
         return res;
     } catch (error) {
